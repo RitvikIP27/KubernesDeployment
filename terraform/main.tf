4b8804e1cb55bec9b9e2d55112ccf729d0a953cf
Ritvik kant <ritvikkantip27@gmail.com>
2026-06-04 02:20:02 +0530

    fixed the t2 to t3 micro t2 no longer supported in aws free tier

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all" {
 
 resource "aws_instance" "app" {
   ami                         = "ami-0f58b397bc5c1f2e8"
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.skillpulse_web_sg.id]
   associate_public_ip_address = true
