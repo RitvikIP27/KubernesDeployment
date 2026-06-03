e2d929ba5c01c5fa890dc5fe5b70f1d78093d941
Ritvik kant <ritvikkantip27@gmail.com>
2026-06-04 02:08:58 +0530

    Created the base infra including a vpc a subnet inside which is an EC2 inst. with secret attached key and security rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,3 +7,95 @@ resource "aws_vpc" "main" {
     Name = "skillpulse-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "skillpulse-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "skillpulse-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "skillpulse-route-table"
+  }
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.main.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_security_group" "skillpulse_web_sg" {
+  name        = "skillpulse-web-sg"
+  description = "Allow SSH, HTTP and HTTPS"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "skillpulse-web-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.skillpulse_web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.skillpulse_web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.skillpulse_web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.skillpulse_web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
+resource "aws_instance" "app" {
+  ami                         = "ami-0f58b397bc5c1f2e8"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.skillpulse_web_sg.id]
+  associate_public_ip_address = true
+
+  key_name = "skillpulse-newkey"
+
+  tags = {
+    Name = "skillpulse-ec2"
+  }
+}
