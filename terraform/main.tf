f8a3eb19b99f120253478f6536b855d2bb480803
Ritvik kant <ritvikkantip27@gmail.com>
2026-07-10 01:58:28 +0530

    Untracked the two resources belonging to terraform itslef for remote state locking and stopped their maamngement once created from terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,22 +27,3 @@ module "compute" {
   elastic_ip_allocation_id = var.elastic_ip_allocation_id
 }
 
-resource "aws_s3_bucket" "statelocker" {
-  bucket = "ritvik-kant-statefile-lock-bucket"
-
-  tags = {
-    Name        = "Terraform state lock"
-    Environment = "dev"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
